6b958e0e8300b5e7cab4daae6151e2ba1b81340a
gebolze <gebolze@gmail.com>
2021-05-04 05:32:14 +0000

    applied initial common schema changes

---
+++ setup-cluster.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo "------------------------------------------------------------------------"
+echo "Setup the local demonstration cluster"
+echo "------------------------------------------------------------------------"
+
+kind create cluster --name k8s-logs \
+  --config kind-cluster-config.yaml
+
+kubectl cluster-info --context kind-k8s-logs
+
+kubectl apply -f \
+  https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+
+echo "------------------------------------------------------------------------"
+echo "Setup elk stack"
+echo "------------------------------------------------------------------------"
+
+kubectl apply -f elk-logging-ns.yaml
+
+kubectl apply -f elk-logging-elasticsearch.yaml
+kubectl rollout status sts/es-cluster --namespace=elk-logging
+
+kubectl apply -f elk-logging-kibana.yaml
+kubectl rollout status deployment/kibana --namespace=elk-logging
+
+echo "please apply the template now"
+kubectl port-forward -n elk-logging service/elasticsearch 9200
+
+kubectl apply -f elk-logging-logstash.yaml
+kubectl rollout status deployment/logstash --namespace=elk-logging
+
+kubectl apply -f elk-logging-ingress.yaml
+
+echo "------------------------------------------------------------------------"
+echo "setup logs collection using fluent-bit"
+echo "------------------------------------------------------------------------"
+
+kubectl apply -f kube-logging-ns.yaml
+kubectl apply -f kube-logging-fluentbit.yaml
+kubectl rollout status daemonset/fluent-bit -n kube-logging
+
+echo "------------------------------------------------------------------------"
+echo "build and publish demo workload with esc common schema logging"
+echo "------------------------------------------------------------------------"
+
+cd demo-web
+docker build . -t gebolze/demo-webapp
+docker push gebolze/demo-webapp
+cd ..
+
+echo "------------------------------------------------------------------------"
+echo "deploy demo workload"
+echo "------------------------------------------------------------------------"
+
+kubectl apply -f demo-webapp.yaml
