25c6d0b06796e6f520e5890a903f538ca234cc4f
Markus Burkert <gebolze@gmail.com>
2021-07-01 08:07:22 +0200

    Added log output control to demo app

--- demo-web/Program.cs
+++ demo-web/Program.cs
@@ -1,19 +1,42 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CommandLine;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using Elastic.CommonSchema.Serilog;
+using Serilog.Formatting;
+using Serilog.Formatting.Display;
+using Serilog.Formatting.Json;
 
 namespace demo_web
 {
+    
+    public enum LoggerMode
+    {
+        Text,
+        Json,
+        ECS
+    }
+
+    public class CommandLineOptions
+    {
+        [Option(Default = LoggerMode.Text, HelpText = "Configures the log output (Text, Json, ECS)", Required = true)]
+        public LoggerMode LoggerMode { get; set; }
+    }
+    
     public class Program
     {
         public static int Main(string[] args)
+        {
+            var results = Parser.Default.ParseArguments<CommandLineOptions>(args);
+            return results.MapResult(
+
+                options => Run(options, args),
+                _ => 1
+            );
+        }
+
+        private static int Run(CommandLineOptions options, string[] args)
         {
             // The initial "bootstrap" logger is able to log errors during start-up. It's completely replaced by the
             // logger configured in `UseSerilog()` below, once configuration and dependency-injection have both been
@@ -25,7 +48,7 @@ namespace demo_web
             try
             {
                 Log.Information("Starting web host");
-                CreateHostBuilder(args).Build().Run();
+                CreateHostBuilder(options.LoggerMode, args).Build().Run();
                 return 0;
             }
             catch (Exception ex)
@@ -38,17 +61,24 @@ namespace demo_web
                 Log.CloseAndFlush();
             }
         }
+        
+        public static IHostBuilder CreateHostBuilder(LoggerMode mode, string[] args)
+        {
+            ITextFormatter formatter = mode switch
+            {
+                LoggerMode.Text => new MessageTemplateTextFormatter("{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}"),
+                LoggerMode.Json => new JsonFormatter(renderMessage: true),
+                LoggerMode.ECS => new EcsTextFormatter(),
+                _ => throw new NotSupportedException()
+            };
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            return Host.CreateDefaultBuilder(args)
                 .UseSerilog((context, services, configuration) => configuration
                     .ReadFrom.Configuration(context.Configuration)
                     .ReadFrom.Services(services)
                     .Enrich.FromLogContext()
-                    .WriteTo.Console(new EcsTextFormatter()))
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    .WriteTo.Console(formatter: formatter))
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        }
     }
 }
