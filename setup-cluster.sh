5a785dbf35e675628b4b0d7c7f0adb793d2e1eec
Markus Burkert <gebolze@gmail.com>
2021-07-07 13:22:35 +0200

    disabled ingress deployment as it's causing problems

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -13,14 +13,14 @@ echo "------------------------------------------------------------------------"
 echo "Setup nginx ingress controller"
 echo "------------------------------------------------------------------------"
 
-kubectl apply -f \
-  https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+#kubectl apply -f \
+#  https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 
 
-kubectl wait --namespace ingress-nginx \
-  --for=condition=ready pod \
-  --selector=app.kubernetes.io/component=controller \
-  --timeout=300s
+#kubectl wait --namespace ingress-nginx \
+#  --for=condition=ready pod \
+#  --selector=app.kubernetes.io/component=controller \
+#  --timeout=300s
 
 echo "------------------------------------------------------------------------"
 echo "Setup elk stack"
